2710fdef6ba65b0e645dbaecaef76a644e0e3c08
kinnerapriyap <kinnerapriyap@gmail.com>
2020-12-27 09:48:38 +0900

    Add const file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'const.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,54 +10,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Is that tone?',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: themeColor,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Is that tone?'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Word:'),
-            _buildList(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildList() => ListView(
-        physics: const NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        children: [
-          _tile('A'),
-          _tile('B'),
-          _tile('C'),
-          _tile('D'),
-        ],
-      );
-
-  ListTile _tile(String title) => ListTile(
-        title: Center(
-            child: Text(title,
-                style: TextStyle(
-                  fontWeight: FontWeight.w300,
-                ))),
-      );
 }
